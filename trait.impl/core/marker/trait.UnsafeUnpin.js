7113b06515c62d1a88141e46232803bd120473fb
artichoke-ci <ci@artichokeruby.org>
2025-04-16 01:36:49 +0000

    deploy: aa4c878fc50790aefbd890ff677bf39f91f64dfd

---
+++ trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["raw_parts",[["impl&lt;T&gt; UnsafeUnpin for <a class=\"struct\" href=\"raw_parts/struct.RawParts.html\" title=\"struct raw_parts::RawParts\">RawParts</a>&lt;T&gt;",1,["raw_parts::RawParts"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[194]}
