4b78b7e398a495f9e6f9a78567411f5008778c9a
artichoke-ci <ci@artichokeruby.org>
2025-06-25 01:44:16 +0000

    deploy: aa4c878fc50790aefbd890ff677bf39f91f64dfd

--- src-files.js
+++ src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["raw_parts",["",[],["lib.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[32]}
+createSrcSidebar('[["raw_parts",["",[],["lib.rs"]]]]');
+//{"start":19,"fragment_lengths":[32]}
